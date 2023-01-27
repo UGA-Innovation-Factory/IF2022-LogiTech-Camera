5950362f16547e218cd379871b2a4644a65f104a
Nicholas Papciak <nicholaspapciak@gmail.com>
2023-01-27 15:02:45 -0500

    :) I love ignoring stuff on git

--- ros-ecosystem/build/mediapipe_ros/ament_cmake_python/mediapipe_ros/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='mediapipe_ros',
-    version='0.1.0',
-    packages=find_packages(
-        include=('mediapipe_ros', 'mediapipe_ros.*')),
-)
